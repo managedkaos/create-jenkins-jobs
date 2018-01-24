b5c901a5780e2c2596bd60e6c01a4442dfeb055b
Michael Jenkins <michael.jenkins@disney.com>
2018-01-23 21:52:50 -0800

    Implement stupid try/except error handlind

--- delete.py
+++ delete.py
@@ -7,4 +7,14 @@ j = Jenkins(os.environ['ENDPOINT'], os.environ['USERNAME'], os.environ['PASSWORD
 
 for team in settings.teams:
     for job in settings.jobs:
-        j.job_delete(team + "-" + job)
+        try:
+            j.job_delete(team + "-" + job)
+        except JenkinsError:
+            print("Error :P")
+
+for job in settings.jobs:
+    try:    
+        j.view_delete(job)
+    except JenkinsError:
+        print("Oops :D")
+
