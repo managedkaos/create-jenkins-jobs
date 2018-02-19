ee17d0dcde142d8fc5ea5335808135b84a874922
Michael Jenkins <michael.jenkins@disney.com>
2018-02-19 03:31:43 -0800

    linting clean up

--- scripts/apache_ivy_base_job.py
+++ scripts/apache_ivy_base_job.py
@@ -1,3 +1,6 @@
+"""
+Apache Ivy Base Job: Creates the base job for building Apache Ivy
+"""
 import os
 from jenkins import Jenkins, JenkinsError
 
@@ -6,13 +9,13 @@ j = Jenkins(os.environ['ENDPOINT'], os.environ['USERNAME'], os.environ['PASSWORD
 
 # open the config.xml
 with open("./xml/jobs/apache-ivy-base-job/config.xml") as config_file:
-    config = config_file.read()
+    CONFIG = config_file.read()
 
 if j.job_exists("apache-ivy"):
     print("\tapache-ivy job exists. skipping...")
 else:
     try:
         print("\tCreating apache-ivy-base-job...")
-        j.job_create("apache-ivy", config)
+        j.job_create("apache-ivy", CONFIG)
     except JenkinsError:
         print("\tCouldn't create the job")
