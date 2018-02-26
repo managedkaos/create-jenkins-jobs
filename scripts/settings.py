c161e538fac6222685e7a1f726bb7e4ab7a6e0a0
Michael Jenkins <michael.jenkins@disney.com>
2018-02-25 16:07:50 -0800

    Update settings

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,4 +1,4 @@
 #teams = ['Blazers', 'Bobcats', 'Bucks', 'Bulls', 'Cavaliers', 'Celtics', 'Clippers', 'Grizzlies', 'Hawks', 'Heat', 'Hornets', 'Jazz', 'Kings', 'Knicks', 'Lakers', 'Magic', 'Mavericks', 'Nets', 'Nuggets', 'Pacers', 'Pistons', 'Raptors', 'Rockets', 'Sixers', 'Spurs', 'Suns', 'Thunder', 'Timberwolves', 'Warriors', 'Wizards']
-teams = ['Clippers', 'Hawks', 'Heat', 'Hornets', 'Lakers', 'Magic', 'Warriors']
+teams = ['Cyclones', 'Clippers', 'Hawks', 'Heat', 'Hornets', 'Lakers', 'Magic', 'Warriors']
 
 jobs = ['BUILD', 'TEST', 'DEPLOY']
