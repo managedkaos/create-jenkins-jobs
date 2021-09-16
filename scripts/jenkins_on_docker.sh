5b0ce219831166176eda29d260786a0fe71d39e9
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-09-15 17:48:29 -0700

    switch ports to use 8080

--- scripts/jenkins_on_docker.sh
+++ scripts/jenkins_on_docker.sh
@@ -2,11 +2,11 @@
 if [ $(ps -elf | grep docker | wc -l) -gt 0 ];
 then
     echo -n "Waiting for Jenkins process to start ."
-    until [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://127.0.0.1:49000) -eq 403      ]; do echo -n '.'; sleep 1; done
+    until [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://127.0.0.1:8080) -eq 403      ]; do echo -n '.'; sleep 1; done
     ADMIN_PASS=$(docker exec jenkins-compose cat /var/jenkins_home/secrets/initialAdminPassword)
     echo
     echo "Jenkins URL:"
-    echo "    http://localhost:49000"
+    echo "    http://localhost:8080"
     echo
     echo "Admin password:"
     echo "    ${ADMIN_PASS}"
